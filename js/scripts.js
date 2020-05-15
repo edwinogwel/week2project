45c983c1eaf050f487c90101220604b83523d28c
edwinogwel <edogwel@gmail.com>
2020-05-15 21:28:34 +0300

    progress update 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
+
 function myfunction(){
+    var days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleAkanNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var date = document.myform.date.value;
-    alert (date)
+    //if it doesn't work comment out next line and try again
+    var gender = document.getElementsByName ("gender");
+    var dayOfBirth = new Date(date);
+    var theDay = dayOfBirth.getDay();
+//*uncomment if needed later*//document.getElementById("message").innerHTML = "The day is " + theDay + "!";
+
+alert(theDay)
 }
+  
