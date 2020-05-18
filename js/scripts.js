fc2d32b1496d997a81525c297ce4030e55a4bc70
edwinogwel <edogwel@gmail.com>
2020-05-18 22:52:12 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-function myfunction() { 
+function myfunction() {
   event.preventDefault();
-      var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-      var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-      var date = document.getElementById("date").value;
-      var gender = document.getElementsByName("gender");
-      var dayOfBirth = new Date(date);
-      var theDay = dayOfBirth.getDay();
-      document.getElementById("message").innerHTML = "The day is " + days[theDay];
-      alert(day)
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var date = document.getElementById("date").value;
+  var gender = document.getElementsByName("gender");
+  var dayOfBirth = new Date(date);
+  var theDay = dayOfBirth.getDay();
+
+  for (var i = 0; i <= gender.length; i++) {
+    if (gender[i].checked) {
+      if (gender[i].value === "Male") {
+        document.getElementById('message').innerHTML = "The gender is " + gender[i].value + " and Akan name is " + maleAkanNames[theDay];
+      }
+      else {
+        document.getElementById('message').innerHTML = "The gender is " + gender[i].value + "and Akan name is " + femaleAkanNames[theDay];
+      }
     }
-  
+    else {
+
+    }
+  }
+}
