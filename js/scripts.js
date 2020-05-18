97e261471482cdbdea18470f221ebebe04a96cef
edwinogwel <edogwel@gmail.com>
2020-05-18 18:41:58 +0300

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-
-function myfunction(){
-    var days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var maleAkanNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var femaleAkanNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    var date = document.myform.date.value;
-    //if it doesn't work comment out next line and try again
-    var gender = document.getElementsByName ("gender");
-    var dayOfBirth = new Date(date);
-    var theDay = dayOfBirth.getDay();
-//*uncomment if needed later*//document.getElementById("message").innerHTML = "The day is " + theDay + "!";
-
-alert(theDay)
-}
+function myfunction() { 
+  event.preventDefault();
+      var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+      var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+      var date = document.getElementById("date").value;
+      var gender = document.getElementsByName("gender");
+      var dayOfBirth = new Date(date);
+      var theDay = dayOfBirth.getDay();
+      document.getElementById("message").innerHTML = "The day is " + days[theDay];
+      alert(day)
+    }
   
